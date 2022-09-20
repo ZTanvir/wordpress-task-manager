92bd791b4040fb804675123e39e2f1a8d1dbc1d2
Ismail Hossen <ihossen016@gmail.com>
2022-09-20 13:01:16 +0600

    code cleaned

--- plugin.php
+++ plugin.php
@@ -1,22 +1,24 @@
- 
 <?php
-function wporg_add_custom_box() {
-   global $post;
+function wporg_add_custom_box()
+{
+    global $post;
     $current_user_id = get_current_user_id();
-    $assigned_user_id =get_post_meta($post->ID, 'assigned_user_id', true);
-    if( $assigned_user_id == $current_user_id ) {
-		add_meta_box(
-			'wporg_box_id',                 
-			'User Box',       
-			'wporg_custom_box_html',   
-			'task'                       
-		);
-    } 
-	}
-    add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
-
-function wporg_custom_box_html() {
-    ?> <input type = "checkbox" id = "checkbox" name = "checkbox" > Complete<?php
+    $assigned_user_id = get_post_meta($post->ID, 'assigned_user_id', true);
+    if ($assigned_user_id == $current_user_id) {
+        add_meta_box(
+            'wporg_box_id',
+            'User Box',
+            'wporg_custom_box_html',
+            'task'
+        );
+    }
 }
-?>
 
+add_action('add_meta_boxes', 'wporg_add_custom_box');
+
+function wporg_custom_box_html()
+{
+    ?> <input type="checkbox" id="checkbox" name="checkbox"> Complete<?php
+}
+
+?>
