0429594a1cb2bc6d445f589ffd137189cb80976b
Ismail Hossen <ihossen016@gmail.com>
2022-09-20 09:14:44 +0600

    code cleaned

--- wordpress-task-manager.php
+++ wordpress-task-manager.php
@@ -12,7 +12,6 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
  */
-// Register Custom Post Type
 
 defined('ABSPATH') || exit;
 
