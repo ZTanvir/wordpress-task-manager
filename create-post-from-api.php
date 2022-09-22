98259ae3a48ed0e135d0eafc7167d58102e99671
Ismail Hossen <ihossen016@gmail.com>
2022-09-22 12:38:17 +0600

    code modified

--- create-post-from-api.php
+++ create-post-from-api.php
@@ -9,33 +9,29 @@
 function get_data_from_api()
 {
     // Test api 
-    $request = wp_remote_get('http://localhost/wordpress/wp-json/form/v1/submit');
+    $request = wp_remote_post('http://localhost/wordpress/wp-json/form/v1/submit');
 
     if (is_wp_error($request)) {
         return false;
     }
 
     $body = wp_remote_retrieve_body($request);
-    $data = json_decode($body);
+    $data = $body;
+//    $data = json_decode($body);
 
     // 
     if (!empty($data)) {
-
-        echo '<ul>';
-        foreach ($data->products as $product) {
-            echo '<li>';
-            //echo '<a href="' . esc_url( $product->info->link ) . '">' . $product->info->title . '</a>';
-            echo $product->info->title;
-            echo '</li>';
-        }
-        echo '</ul>';
+        echo $data;
+    } else {
+        echo "Data not received";
     }
+
     // Gather post data.
     $create_post = array(
         // api title
-        'post_title' => 'My post',
+        'post_title' => 'Title',
         // api content
-        'post_content' => 'This is my post.',
+        'post_content' => 'description',
         // 'post_status'   => 'publish',
         // 'post_author'   => 1,
         'post_category' => array(8, 39),
@@ -49,4 +45,4 @@ function get_data_from_api()
 
 add_action('wp_head', 'get_data_from_api');
 
-?>
+
