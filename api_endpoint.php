5a85fddba2b76c35105fc4cdaf7d809e59d07e3d
Ferdowsi Marufa <marufa.fm@gmail.com>
2022-09-22 09:41:40 +0600

    code added

---
+++ api_endpoint.php
@@ -0,0 +1,15 @@
+<?php
+
+add_action ( 'rest_api_init', "register_rest_api");
+ 
+function register_rest_api() {
+   register_rest_route( 'form/v1','submit',array(
+           'methods'=> 'POST',
+           'callback'=>  'my_awesome_func'
+   ));
+}
+function my_awesome_func() {
+    
+}
+
+?>
