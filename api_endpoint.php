49eb1de69bd012fb8a6cbce5ce93a3d889bf9ae5
Ismail Hossen <ihossen016@gmail.com>
2022-09-22 12:37:36 +0600

    api register code modified

--- api_endpoint.php
+++ api_endpoint.php
@@ -2,17 +2,34 @@
 
 add_action('rest_api_init', "register_rest_api");
 
+// Register REST API function
 function register_rest_api()
 {
     register_rest_route('form/v1', 'submit', array(
-        'methods' => WP_REST_Server::CREATABLE,
+        'methods' => 'POST',
         'callback' => 'my_awesome_func'
     ));
 }
 
-function my_awesome_func()
+// Callback function for REST API register
+function my_awesome_func($request)
 {
+    $data = array();
+    $title = $request->get_param('title');
+    $description = $request->get_param('description');
 
+    if (isset($title) && isset($description)) {
+        $data['status'] = 'OK';
+        $data ['received_data'] = array(
+            'title' => $title,
+            'description' => $description
+        );
+        $data['message'] = "Data sent successfully";
+    } else {
+        $data['status'] = 'Failed';
+        $data['message'] = 'Data Missing';
+    }
+    
+    return $data;
 }
 
-?>
